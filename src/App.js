e5a63b30e2e223ddb9b3cd4ac614ebfb935746e5
Ege Ozler <ozlerrege@gmail.com>
2023-03-15 13:25:23 -0400

    changed exact route

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
           <Layout />
 
           <Routes>
-            <Route exact path="/" element={<Login />} />
+            <Route path="/" element={<Login />} />
             <Route path="/signup" element={<SignUp />} />
 
             <Route path="/homepage" element={<HomePage />} />
