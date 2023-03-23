12e8aa580b92a5b6282a533e1d07558ace51e070
Ege Ozler <ozlerrege@gmail.com>
2023-03-23 11:57:38 -0400

    added hashrouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createHashRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
-import "./styles/index.css";
 
 const router = createHashRouter([
   {
